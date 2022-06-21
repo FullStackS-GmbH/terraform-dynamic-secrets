364f03fef479974ba3b495b28b71dd2ed713a541
Sebastian Mangelkramer <sebastian.mangelkramer@fullstacks.eu>
2022-06-21 15:54:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,6 +37,8 @@ resource "vault_aws_secret_backend_role" "ec2_admin_role" {
 IAM
 }
 
+
+
 # resource "vault_aws_secret_backend_role" "s3_admin_role" {
 #   backend         = vault_aws_secret_backend.aws_secret.path
 #   name            = "s3-admin-role"
